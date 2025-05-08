e247a8e0fc2253dbc46405300fbb7ab4ec14baf7
agi2019 <49863588+agi2019@users.noreply.github.com>
2025-05-08 15:22:18 +1000

    Update policy_priority_inference.py

--- source_code/policy_priority_inference.py
+++ source_code/policy_priority_inference.py
@@ -333,9 +333,9 @@ def run_ppi(I0, alphas, alphas_prime, betas, A=None, R=None, bs=None, qm=None, r
     # Prevent null and zero allocations	    	    
     ## P0 = Bs[:,0].sum()*P0/P0.sum()
     P0 = np.nan_to_num(P0, nan=1e-12)
-    Bs[Bs==0] = 10e-12
+    #Bs[Bs==0] = 10e-12
     P0 = np.nan_to_num(P0, nan=1e-12)
-    P0[P0==0] = 10e-12
+    #P0[P0==0] = 10e-12
     print("P0 keempat", P0)
 		    
     ## INSTANTIATE ALL VARIABLES AND CREATE CONTAINERS TO STORE DATA
