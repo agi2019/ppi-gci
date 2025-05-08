c65a950cf55d6acd74e19835c1b39a6b274a69f0
agi2019 <49863588+agi2019@users.noreply.github.com>
2025-05-08 12:36:24 +1000

    Update policy_priority_inference.py

--- source_code/policy_priority_inference.py
+++ source_code/policy_priority_inference.py
@@ -372,7 +372,7 @@ def run_ppi(I0, alphas, alphas_prime, betas, A=None, R=None, bs=None, qm=None, r
         X = X + sign*np.abs(changeF) # determine current action
         assert np.sum(np.isnan(X)) == 0, 'X has invalid values!'
         C = P/(1 + np.exp(X)) # map action into contribution
-        assert np.sum(np.isnan(C)) == 0, 'C has invalid values!'
+        assert np.sum(np.isnan(C)) == 0, 'C has invalid values bro!'
         assert np.sum(P < C)==0, 'C cannot be larger than P!'
         signt = sign.copy() # update previous signs
         changeFt = changeF.copy() # update previous changes in benefits
